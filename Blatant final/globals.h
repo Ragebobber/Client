8d42a7cdd7ace1d02febd4ccb1f6fad50e1144ba
Nik <51878823+Ragebobber@users.noreply.github.com>
2023-08-21 16:12:10 +0300

    Update globals.h

--- Blatant final/globals.h
+++ Blatant final/globals.h
@@ -1,13 +1,5 @@
 #pragma once
 
-
-//extern std::shared_ptr<HttpStruct> httpStruct ;
-//extern std::unique_ptr<RestClient> restClient ;
-//extern std::unique_ptr<DtoImpl> dto;
-//extern std::unique_ptr<ApiConnector> apiConnector;
-//extern std::unique_ptr<Security> security;
-//extern std::unique_ptr<Injection> injection;
-
 extern HttpStruct* httpStruct;
 extern NtFunctions* nt;
 extern RestClient* restClient;
