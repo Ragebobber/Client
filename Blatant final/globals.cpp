011c4f6bd18fb176fe0505ea4fc7822f76c7c524
Nik <51878823+Ragebobber@users.noreply.github.com>
2023-08-21 16:12:01 +0300

    Update globals.cpp

--- Blatant final/globals.cpp
+++ Blatant final/globals.cpp
@@ -1,13 +1,5 @@
 #include "SDK.h"
 
-//
-//std::shared_ptr<HttpStruct> httpStruct(new HttpStruct );
-//std::unique_ptr<RestClient> restClient(new RestClient );
-//std::unique_ptr<DtoImpl> dto ( new DtoImpl );
-//std::unique_ptr<ApiConnector> apiConnector ( new ApiConnector );
-//std::unique_ptr<Security> security ( new Security );
-//std::unique_ptr<Injection> injection ( new Injection );
-
  HttpStruct* httpStruct = new HttpStruct();
  NtFunctions* nt = new NtFunctions ( );
  RestClient* restClient = new RestClient ( );
